6f8ab5b4d22fc5209f41672b4e0a8396f5955a25
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-11-20 19:20:25 -0800

    create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from '../components/footer/Footer'
+import Links from '../components/links/Links'
+import SongPreview from '../components/songPreview/SongPreview'
+import './home.css'
+
+export default function Home() {
+    return (
+        <div className="home">
+            <div className="logo">Novacane</div>
+            <SongPreview/>
+            <div className="homeLinks">
+                
+                <Links/>  
+                <Footer/>
+            </div>
+        </div>
+    )
+}
