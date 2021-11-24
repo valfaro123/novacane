de134f6a6e681a869f82d5c01aaea8f21a869ccd
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-11-23 23:59:56 -0800

    update link info and styles

--- src/components/links/LinkContent.js
+++ src/components/links/LinkContent.js
@@ -1,22 +1,33 @@
 export const LinkContent = [
     {
-        icon: 'fab fa-twitter',
-        url: 'https://twitter.com/novacanesounds',
-        label: 'Twitter',
+        icon: 'fab fa-spotify',
+        url: 'https://open.spotify.com/artist/42FeiTG6C9aGFEjn6ie3JH?si=l3sC8MOcQZiOHcVNd70G6w&dl_branch=1&nd=1',
+        label: 'Spotify',
     },
     {
-        icon: 'fab fa-instagram',
-        url: 'https://www.instagram.com/novacanesounds/',
-        label: 'Instagram',
+        icon: 'fab fa-apple',
+        url: 'https://music.apple.com/us/artist/novacane/1465840760',
+        label: 'Apple Music',
     },
-    {
-        icon: 'fab fa-tiktok',
-        url: 'https://www.tiktok.com/@novacanesounds',
-        label: 'TikTok',
-    },
+    
     {
         icon: 'fab fa-soundcloud',
         url: 'https://soundcloud.com/novacanesounds',
         label: 'SoundCloud',
     },
+    { 
+        icon: 'fab fa-instagram',
+        url: 'https://instagram.com/novacanesounds',
+        label: 'Instagram'
+    },
+    {
+        icon: 'fab fa-twitter',
+        url: 'https://twitter.com/novacanesounds',
+        label: 'Twitter'
+    },
+    {
+        icon: 'fab fa-tiktok',
+        url: 'www.tiktok.com/@novacanesounds',
+        label: 'TikTok' 
+    }
 ]
