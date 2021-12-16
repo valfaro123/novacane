e7a2ca7cbb9f718848301d460ddd45cca9e2476d
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-12-16 13:09:50 -0800

    remove footer

--- src/components/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './footer.css'
-export default function Footer() {
-    return (
-        <div className="footer">
-            <span>Test Footer 2021</span>
-        </div>
-    )
-}
