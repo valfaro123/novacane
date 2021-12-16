deb6315d29f0952dca8c5d799cb20202d4be12bc
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-12-16 13:11:10 -0800

    update embed song player

--- src/components/songPreview/SongPreview.jsx
+++ src/components/songPreview/SongPreview.jsx
@@ -3,7 +3,8 @@ import './songPreview.css'
 export default function SongPreview() {
     return (
         <div className="songPreview">
-            <iframe className="player" src="https://open.spotify.com/embed/track/2GE1I0MRE0PzIN1Vdc6lbE?utm_source=generator&theme=0"  frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            
+            <iframe title="songPlayer" className="player" src="https://open.spotify.com/embed/track/6Kc8o87oIyExCYhBTferEV?utm_source=generator" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
         </div>
     )
 }
