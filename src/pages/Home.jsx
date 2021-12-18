de5633de7be36be6623bb185ed2dd43370375542
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-12-17 19:31:03 -0800

    add logo svg component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Links from '../components/links/Links'
 import SongPreview from '../components/songPreview/SongPreview'
+import Logo from '../svg/Logo'
 import './home.css'
+
 
 export default function Home() {
     return (
         <div className="home">
-            <div className="logo">Novacane</div>
+            <Logo/>
             <SongPreview/>
             <div className="homeLinks">
                 <Links/> 
