6a52742a9b0c4c12cbb7ab501c72d1708a84c72f
valfaro123 <valfaro6@horizon.csueastbay.edu>
2022-02-10 21:50:30 -0800

    fix mobile responsiveness

--- src/components/songPreview/SongPreview.jsx
+++ src/components/songPreview/SongPreview.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './songPreview.css'
 export default function SongPreview() {
-    //temp add youtube inteview 
+    //temp add
     return (
-        <div className="songPreview">
+        <div className="video-container">
             {/* <iframe title="songPlayer" className="player" src="https://open.spotify.com/embed/track/6Kc8o87oIyExCYhBTferEV?utm_source=generator" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe> */}
-
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/XzTB04lwIaE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           
+            
+            <iframe  src="https://www.youtube.com/embed/XzTB04lwIaE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
